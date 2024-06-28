52e8d8ed03509e9a11251cf2af4b53cdee8729e8
Mondli <mondliolmokoka@gmail.com>
2024-06-28 13:46:44 +0200

    Update script.js
    Added email validation

--- LandingPage2/script.js
+++ LandingPage2/script.js
@@ -1,4 +1,8 @@
 document.getElementById('subscription-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    window.location.href = 'preferences.html';
+    var email = document.getElementById('email').value;
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(email)){
+        alert('Please enter a valid email address.');
+        event.preventDefault();
+    }
 });
